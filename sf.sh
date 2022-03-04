18fde84ad63bfbc5ef1e1561f860cf810c5263a3
FireMe <63303853+Mr-Gabut@users.noreply.github.com>
2022-03-05 00:28:51 +0700

    Create sf.sh

---
+++ sf.sh
@@ -0,0 +1,18 @@
+pkg update 
+pkg upgrade
+echo "Intsalling Python" 
+sleep 2
+pkg install python -y
+pkg install python2 -y
+pkg install curl git python2
+echo "Installing Mechanize And Requests"
+sleep 2
+pip2 install mechanize
+pip2 install requests
+echo "Thanks Mr-Gabut"
+sleep 1
+echo "Support My Channel!!"
+sleep 1
+xdg-open https://www.youtube.com/channel/UCEg3T9hyhlkDkTPf_kGo4ow
+sleep 2
+python2 main.py
